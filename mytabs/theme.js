6cca097be4e1d96e6a30b5df2bc23d453e7ad7eb
Kamil <kempa3@gmail.com>
2025-06-09 01:14:21 +0200

    Add column count support

--- mytabs/theme.js
+++ mytabs/theme.js
@@ -1,5 +1,6 @@
 (async function(){
-  const { theme = 'light', tileWidth = 250 } = await browser.storage.local.get(['theme','tileWidth']);
+  const { theme = 'light', tileWidth = 250, cols = 3 } = await browser.storage.local.get(['theme','tileWidth','cols']);
   document.body.dataset.theme = theme;
   document.documentElement.style.setProperty('--tile-width', tileWidth + 'px');
+  document.documentElement.style.setProperty('--cols', cols);
 })();
