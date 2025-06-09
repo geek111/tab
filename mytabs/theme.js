204aff84173755e982b3bab0b464519de14acd1d
Kamil <kempa3@gmail.com>
2025-06-09 20:46:44 +0200

    Add tile scale option

--- mytabs/theme.js
+++ mytabs/theme.js
@@ -1,9 +1,11 @@
 (async function(){
-  let { theme = 'light', tileWidth = 250, cols = 3 } = await browser.storage.local.get(['theme','tileWidth','cols']);
+  let { theme = 'light', tileWidth = 250, tileScale = 1, cols = 3 } = await browser.storage.local.get(['theme','tileWidth','tileScale','cols']);
 
   function apply(){
     document.body.dataset.theme = theme;
-    document.documentElement.style.setProperty('--tile-width', tileWidth + 'px');
+    const width = tileWidth * tileScale;
+    document.documentElement.style.setProperty('--tile-width', width + 'px');
+    document.documentElement.style.setProperty('--tile-scale', tileScale);
     document.documentElement.style.setProperty('--cols', cols);
     if (document.body.classList.contains('full')) {
       document.body.style.removeProperty('width');
@@ -16,6 +18,7 @@
     if (area === 'local') {
       if (changes.theme) theme = changes.theme.newValue;
       if (changes.tileWidth) tileWidth = changes.tileWidth.newValue;
+      if (changes.tileScale) tileScale = changes.tileScale.newValue;
       if (changes.cols) cols = changes.cols.newValue;
       apply();
     }
