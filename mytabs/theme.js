c4df47b50f191926a7da0e57d12db03a4a49ef19
Kamil <kempa3@gmail.com>
2025-06-09 15:13:24 +0200

    Remove columns option and apply dynamic width

--- mytabs/theme.js
+++ mytabs/theme.js
@@ -1,10 +1,24 @@
 (async function(){
-  const { theme = 'light', tileWidth = 250, cols = 3 } = await browser.storage.local.get(['theme','tileWidth','cols']);
-  document.body.dataset.theme = theme;
-  document.documentElement.style.setProperty('--tile-width', tileWidth + 'px');
-  document.documentElement.style.setProperty('--cols', cols);
-  if (document.body.classList.contains('full')) {
-    const width = tileWidth * cols;
-    document.body.style.width = width + 'px';
+  let { theme = 'light', tileWidth = 250, cols = 3 } = await browser.storage.local.get(['theme','tileWidth','cols']);
+
+  function apply(){
+    document.body.dataset.theme = theme;
+    document.documentElement.style.setProperty('--tile-width', tileWidth + 'px');
+    document.documentElement.style.setProperty('--cols', cols);
+    if (document.body.classList.contains('full')) {
+      const width = tileWidth * cols;
+      document.body.style.width = width + 'px';
+    }
   }
+
+  apply();
+
+  browser.storage.onChanged.addListener((changes, area) => {
+    if (area === 'local') {
+      if (changes.theme) theme = changes.theme.newValue;
+      if (changes.tileWidth) tileWidth = changes.tileWidth.newValue;
+      if (changes.cols) cols = changes.cols.newValue;
+      apply();
+    }
+  });
 })();
